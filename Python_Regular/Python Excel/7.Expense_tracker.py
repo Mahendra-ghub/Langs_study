11c90f6de31df66e42264dc50c5af88b98dac984
velpula Mahendra <mahendramahi372387@gmail.com>
2024-11-16 16:16:39 +0530

    finish

--- Python_Regular/Python Excel/7.Expense_tracker.py
+++ Python_Regular/Python Excel/7.Expense_tracker.py
@@ -4,17 +4,20 @@ import csv
 dt = date.today()
 df = dt.strftime("%d/%m/%y")
 samplecsv = "sample.csv"
+expenses_name = []
 expenses_data = []
+
 stopped = False
 
-with open(samplecsv, "w",newline="") as file:
+with open(samplecsv, "a",newline="") as file:
     writer =csv.writer(file)
     while not stopped:
-        enter_expenses = int(input("Enter a expenses(type 0 to stop): "))
+        expenses_name= str(input("spended for(type 0 to stop): "))
+        enter_expenses = int(input("Enter a expenses: "))
         if enter_expenses==0:
             stopped = True
         else:
-            writer.writerow([df, enter_expenses])
+            writer.writerow([df,expenses_name, enter_expenses])
             expenses_data.append(enter_expenses)
 print(f"expenses are:{expenses_data}")
 print(f"total expenses is: {sum(expenses_data)}")
